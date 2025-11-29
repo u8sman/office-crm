b0e6edf5d3dabd2d01feb5245682a6be47ec2f1c
Usman <u8sman@gmail.com>
2025-11-29 16:59:33 +0500

    THEME INSTALL COMPLETED

--- static/common/js/vendor/paragraph_textareas.js
+++
@@ -1,5 +0,0 @@
-bkLib.onDomLoaded(function() { nicEditors.allTextAreas({iconsPath : '/static/common/js/vendor/nicEditorIcons.gif',
-    buttonList : ['fontSize','fontFamily','bold','italic',
-        'underline','strikeThrough','left','center','right','justify',
-        'ol','ul','subscript','superscript','hr','link','unlink','forecolor']
-}) });
