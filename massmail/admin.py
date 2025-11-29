ed599fcc9398fb0f756c6fbc8ff181c201c7b6ae
Usman <u8sman@gmail.com>
2025-11-29 19:32:10 +0500

    added home page

--- massmail/admin.py
+++ massmail/admin.py
@@ -2,168 +2,93 @@ from django import forms
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
-from unfold.admin import ModelAdmin
-
 from crm.site.crmmodeladmin import CrmModelAdmin
 from crm.site.crmadminsite import crm_site
-from massmail.models import (
-    EmailAccount,
-    EmlMessage,
-    EmlAccountsQueue,
-    MailingOut,
-    MassContact,
-    Signature,
-)
-from massmail.site import (
-    emailaccountadmin,
-    emlmessageadmin,
-    mailingoutadmin,
-    signatureadmin,
-)
+from massmail.models import EmailAccount
+from massmail.models import EmlMessage
+from massmail.models import EmlAccountsQueue
+from massmail.models import MailingOut
+from massmail.models import MassContact
+from massmail.models import Signature
+from massmail.site import emailaccountadmin
+from massmail.site import emlmessageadmin
+from massmail.site import mailingoutadmin
+from massmail.site import signatureadmin
 
 
-@admin.register(EmailAccount)
 class EmailAccountAdmin(emailaccountadmin.EmailAccountAdmin):
-    """
-    Email account admin in the default site, extending the project-specific
-    EmailAccountAdmin but giving it an explicit Unfold-styled field layout.
-    """
-
     fieldsets = (
-        (
-            None,
-            {
-                "fields": (
-                    "name",
-                    "main",
-                    "massmail",
-                    "do_import",
-                    "email_host",
-                    "imap_host",
-                    "email_host_user",
-                    "email_host_password",
-                    "email_app_password",
-                    "email_port",
-                    "from_email",
-                    "email_use_tls",
-                    "email_use_ssl",
-                    "email_imail_ssl_certfile",
-                    "email_imail_ssl_keyfile",
-                    "refresh_token",
-                )
-            },
-        ),
-        (
-            _("Service information"),
-            {
-                "fields": (
-                    "report",
-                    "today_date",
-                    "today_count",
-                    "start_incoming_uid",
-                    "start_sent_uid",
-                    "last_import_dt",
-                )
-            },
-        ),
-        (
-            _("Additional information"),
-            {
-                "fields": (
-                    ("owner", "co_owner", "modified_by"),
-                    ("creation_date", "update_date"),
-                )
-            },
-        ),
+        (None, {
+            'fields': (
+                'name', 'main', 'massmail', 'do_import',
+                'email_host', 'imap_host', 'email_host_user',
+                'email_host_password', 'email_app_password', 'email_port',
+                'from_email', 'email_use_tls', 'email_use_ssl',
+                'email_imail_ssl_certfile', 'email_imail_ssl_keyfile',
+                'refresh_token'
+            )
+        }),
+        (_('Service information'), {
+            'fields': (
+                'report', 'today_date', 'today_count',
+                'start_incoming_uid', 'start_sent_uid', 'last_import_dt'
+            )
+        }),
+        (_('Additional information'), {
+            'fields': (
+                ('owner', 'co_owner', 'modified_by'), 'department',
+                ('creation_date', 'update_date',)
+            )
+        }),
     )
 
 
-@admin.register(EmlMessage)
 class MessageAdmin(emlmessageadmin.EmlMessageAdmin):
-    """
-    EmlMessage admin in the default site.
-    """
-
     readonly_fields = (
-        "modified_by",
-        "signature_preview",
-        "msg_preview",
+        'modified_by',
+        'signature_preview',
+        'msg_preview'
     )
 
     # -- ModelAdmin methods -- #
 
     def get_form(self, request, obj=None, **kwargs):
-        """
-        Strip JS media from the parent form (keeps your original logic).
-        """
         form = super().get_form(request, obj, **kwargs)
         form.media = forms.Media(js=[])
         return form
 
 
-@admin.register(Signature)
 class SignatureAdmin(signatureadmin.SignatureAdmin):
-    """
-    Signature admin in the default site.
-    """
-
     readonly_fields = (
-        "modified_by",
-        "update_date",
-        "creation_date",
-        "preview",
+        'modified_by', 'update_date', 'creation_date', 'preview'
     )
 
 
-@admin.register(MailingOut)
 class MailingOutAdmin(mailingoutadmin.MailingOutAdmin):
-    """
-    MailingOut admin in the default site, with explicit Unfold-style fieldsets.
-    """
-
     exclude = []
-    readonly_fields = list()  # you can add ('recipients_number',) back here if needed
-
+    readonly_fields = list()  # ('recipients_number',)
     fieldsets = (
-        (
-            None,
-            {
-                "fields": (
-                    ("name", "status"),
-                    ("content_type", "recipients_number"),
-                    "message",
-                    "report",
-                    "recipient_ids",
-                    "successful_ids",
-                    "failed_ids",
-                    ("owner", "modified_by"),
-                )
-            },
-        ),
+        (None, {
+            'fields': (
+                ('name', 'status'),
+                ('content_type', 'recipients_number'),
+                'message', 'report',
+                'recipient_ids', 'successful_ids', 'failed_ids',
+                ('owner', 'modified_by'),
+            )
+        }),
     )
 
 
-@admin.register(MassContact)
 class MassContactAdmin(CrmModelAdmin):
-    """
-    Admin for MassContact, based on project CrmModelAdmin so it inherits
-    all CRM-specific behavior but still looks like Unfold.
-    """
-
-    list_display = (
-        "content_object",
-        "content_type",
-        "object_id",
-        "email_account",
-        "massmail",
-    )
+    list_display = ('content_object', 'content_type', 'object_id', 'email_account', 'massmail',)
     list_filter = (
-        ("email_account__owner", admin.RelatedOnlyFieldListFilter),
-        "massmail",
-        ("content_type", admin.RelatedOnlyFieldListFilter),
-        ("email_account", admin.RelatedOnlyFieldListFilter),
+        ('email_account__owner', admin.RelatedOnlyFieldListFilter),
+        'massmail',
+        ('content_type', admin.RelatedOnlyFieldListFilter),
+        ('email_account', admin.RelatedOnlyFieldListFilter),
     )
-    search_fields = ["object_id"]
+    search_fields = ['object_id', ]
     save_on_top = False
 
     # -- ModelAdmin methods -- #
@@ -172,21 +97,15 @@ class MassContactAdmin(CrmModelAdmin):
         return self.list_filter
 
     def get_queryset(self, request):
-        # Use the CrmModelAdmin / ModelAdmin get_queryset properly
-        return super().get_queryset(request)
+        return super(admin.ModelAdmin, self).get_queryset(request)
 
 
-@admin.register(EmlAccountsQueue)
-class EmlAccountsQueueAdmin(ModelAdmin):
-    """
-    Simple Unfold ModelAdmin for EmlAccountsQueue so it also gets the theme.
-    """
-
-    # No custom options; Unfold will still style the change list / forms.
-    pass
-
-
-# --- CRM custom admin site registrations (unchanged) --- #
+admin.site.register(Signature, SignatureAdmin)
+admin.site.register(EmlMessage, MessageAdmin)
+admin.site.register(MailingOut, MailingOutAdmin)
+admin.site.register(EmailAccount, EmailAccountAdmin)
+admin.site.register(MassContact, MassContactAdmin)
+admin.site.register(EmlAccountsQueue)
 
 crm_site.register(EmailAccount, emailaccountadmin.EmailAccountAdmin)
 crm_site.register(EmlMessage, emlmessageadmin.EmlMessageAdmin)
